31cba915dfb188750dd57b71a3394ff7e810b56a
daffashiddiq <daffashiddiq@gmail.com>
2021-07-03 16:47:10 +0700

    Initial Commit

---
+++ BetterRest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BetterRest
+//
+//  Created by Daffashiddiq on 03/07/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
